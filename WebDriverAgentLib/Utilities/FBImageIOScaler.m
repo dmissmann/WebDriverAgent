5879ef9ee7a3aa4997b7760dd0603d2a78e9fb4e
David Missmann <david.missmann@saucelabs.com>
2019-01-15 10:18:02 +0100

    updated file headers

--- WebDriverAgentLib/Utilities/FBImageIOScaler.m
+++ WebDriverAgentLib/Utilities/FBImageIOScaler.m
@@ -1,10 +1,11 @@
-//
-//  FBImageIOScaler.m
-//  WebDriverAgentLib
-//
-//  Created by David on 14.01.19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import "FBImageIOScaler.h"
 #import <ImageIO/ImageIO.h>
